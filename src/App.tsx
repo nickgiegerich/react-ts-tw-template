fc429190c5b222bf5a33ac0aeac8a7682e02d1d3
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2022-01-29 10:17:16 -0700

    first go, maybe..

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col w-screen h-screen justify-center items-center text-center px-5 text-4xl font-mono bg-slate-900 text-lime-700'>
+      Delete me and start coding with Typescript and Tailwind!
     </div>
   );
 }
